91739b8444d4a5b50876097bed5372016daadb1b
justin <justin@fluidinvestmentgroup.com>
2024-06-27 23:42:25 -0400

    Update endpoint URL and add error handling

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
+import os
 from flask import Flask, jsonify, request
 import requests
-import os
 
 app = Flask(__name__)
 
-@app.route('/flight-info/<carrierCode>/<flightNumber>', methods=['GET'])
-def get_flight_info(carrierCode, flightNumber):
+@app.route('/flight-info/<carrier>/<flight>', methods=['GET'])
+def get_flight_info(carrier, flight):
     app_id = os.getenv('FLIGHTSTATS_APP_ID')
     app_key = os.getenv('FLIGHTSTATS_APP_KEY')
-    url = f"https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/{carrierCode}/{flightNumber}/dep?appId={app_id}&appKey={app_key}"
-    
+    url = f'https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/{carrier}/{flight}?appId={app_id}&appKey={app_key}'
     response = requests.get(url)
-    print(f"URL: {url}")
-    print(f"Response: {response.text}")
-
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({'error': response.json()}), response.status_code
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(debug=True)
