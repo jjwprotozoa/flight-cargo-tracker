fe0557f40359cf7d3ae8fb4807938d6066fb4888
justin <justin@fluidinvestmentgroup.com>
2024-06-27 22:18:12 -0400

    Add root route

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask, jsonify, request 
-import requests 
-import os 
- 
-app = Flask(__name__) 
- 
-FLIGHTSTATS_APP_ID = os.getenv('FLIGHTSTATS_APP_ID') 
-FLIGHTSTATS_APP_KEY = os.getenv('FLIGHTSTATS_APP_KEY') 
-SOUTHWEST_CARGO_API_KEY = os.getenv('SOUTHWEST_CARGO_API_KEY') 
- 
-def get_flight_info(flight_number): 
-    response = requests.get(flightstats_url) 
-    return jsonify(response.json()) 
- 
-def get_cargo_info(tracking_id): 
-    cargo_url = f'https://rapidapi.p.rapidapi.com/cargo/v1/track/{tracking_id}' 
-    headers = { 
-        'Authorization': f'Bearer {SOUTHWEST_CARGO_API_KEY}', 
-        'x-rapidapi-host': 'sw-cargo-tracking.p.rapidapi.com', 
-        'x-rapidapi-key': SOUTHWEST_CARGO_API_KEY 
-    } 
-    response = requests.get(cargo_url, headers=headers) 
-    return jsonify(response.json()) 
- 
-if __name__ == '__main__': 
-    app.run(debug=True) 
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flight Cargo Tracker is running!"
+
+@app.route('/flight-info/<flight_number>', methods=['GET'])
+def get_flight_info(flight_number):
+    # Replace with actual API call to fetch flight information
+    response = requests.get(f"https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/{flight_number}?appId=YOUR_FLIGHTSTATS_APP_ID&appKey=YOUR_FLIGHTSTATS_APP_KEY")
+    return jsonify(response.json())
+
+@app.route('/cargo-info/<waybill_number>', methods=['GET'])
+def get_cargo_info(waybill_number):
+    # Replace with actual API call to fetch cargo information
+    response = requests.get(f"https://rapidapi.com/developer/authorization/default-application_9293627?trackingId={waybill_number}&apikey=YOUR_SOUTHWEST_CARGO_API_KEY")
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(debug=True)
