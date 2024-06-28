545a22fb3b3f0da714d2dcd6a2a7b79e417b4338
justin <justin@fluidinvestmentgroup.com>
2024-06-27 22:22:37 -0400

    Import requests library to fix NameError

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import requests  # Import the requests library
 
 app = Flask(__name__)
 
@@ -6,16 +7,14 @@ app = Flask(__name__)
 def home():
     return "Flight Cargo Tracker is running!"
 
-@app.route('/flight-info/<flight_number>', methods=['GET'])
+@app.route('/flight-info/<flight_number>')
 def get_flight_info(flight_number):
-    # Replace with actual API call to fetch flight information
     response = requests.get(f"https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/{flight_number}?appId=YOUR_FLIGHTSTATS_APP_ID&appKey=YOUR_FLIGHTSTATS_APP_KEY")
     return jsonify(response.json())
 
-@app.route('/cargo-info/<waybill_number>', methods=['GET'])
+@app.route('/cargo-info/<waybill_number>')
 def get_cargo_info(waybill_number):
-    # Replace with actual API call to fetch cargo information
-    response = requests.get(f"https://rapidapi.com/developer/authorization/default-application_9293627?trackingId={waybill_number}&apikey=YOUR_SOUTHWEST_CARGO_API_KEY")
+    response = requests.get(f"https://api.southwestcargo.com/tracking/{waybill_number}?apiKey=YOUR_SOUTHWEST_CARGO_API_KEY")
     return jsonify(response.json())
 
 if __name__ == '__main__':
